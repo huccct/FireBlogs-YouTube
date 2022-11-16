5279c55f829ef73c5ded1ef43fcab7b5b96bea70
huccct <2972223145@qq.com>
2022-11-16 23:40:48 +0800

    Update Day2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Home",
-    components: {},
-  };
+import BlogPost from '../components/BlogPost'
+export default {
+  name: "Home",
+  components: { BlogPost },
+  data () {
+    return {
+      welcomeScreen: {
+        title: 'Welcome',
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more.Register tody to never miss a post!",
+        welcomeScreen: true,
+        photo: 'coding'
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filler Title!",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "beautiful-stories"
+        },
+        {
+          title: "This is a Filler Title!",
+          blogHTML: "This is a filter blog post title!",
+          blogCoverPhoto: "designed-for-everyone"
+        }
+      ]
+    }
+  }
+}
 </script>

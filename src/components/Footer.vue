eddfd0b03320476cd1590bf2b5e7c6383c8ca08c
huccct <2972223145@qq.com>
2022-11-19 23:51:27 +0800

    Update Day4

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="left">
         <div class="col-1">
-          <router-link class="header" :to="{name:'Home'}">FireBlogs</router-link>
+          <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
           <ul>
             <li>
               <a href="#">
@@ -23,10 +23,10 @@
         </div>
         <div class="col-2">
           <ul>
-            <!-- <router-link class="link" :to="{ name:'home' }">Home</router-link> -->
-            <!-- <router-link class="link" :to="{ name:'blogs' }">Blogs</router-link> -->
-            <!-- <router-link v-if="user" class="link" :to="{ name:'newpost' }">Create Post</router-link> -->
-            <!-- <router-link v-if="!user" class="link" :to="{ name:'login' }">Login In / Register</router-link> -->
+            <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+            <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
+            <!-- <router-link v-if="user" class="link" :to="{ name: 'newpost' }">Create Post</router-link> -->
+            <!-- <router-link v-if="!user" class="link" :to="{ name: 'login' }">Login In / Register</router-link> -->
           </ul>
         </div>
       </div>
